eafdb04274736d378b67abab5e4ee5b5d5bd1439
Steven Ayers <steveay99@gmail.com>
2017-02-25 20:53:27 +0000

    Adding a load of the folder structure

---
+++ Puppet/manifests/site.pp
@@ -0,0 +1,12 @@
+if versioncmp($::puppetversion,'3.6.1') >= 0 {
+
+  $allow_virtual_packages = hiera('allow_virtual_packages',false)
+
+  Package {
+    allow_virtual => $allow_virtual_packages,
+  }
+}
+
+node default {
+  hiera_include('classes', 'fnc_core::roles::default')
+}
