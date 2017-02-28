e2d2eb94f0c84171a2484bca1b68cb5e1a9345ed
Steven Ayers <steveay99@gmail.com>
2017-02-28 15:36:22 +0000

    Getting Hiera to work

--- puppet/modules/cervidae/manifests/roles/default.pp
+++ puppet/modules/cervidae/manifests/roles/default.pp
@@ -1,6 +1,6 @@
 class cervidae::roles::default {
   $server_type = hiera('classes', 'fnc_core::roles::default')
 
-  notice("Installed started on ${::fqdn} as class ${server_type} (${server_facts::server_type}).")
+  notice("Installed started on ${::fqdn} as class ${::server_type}.")
 
 }
