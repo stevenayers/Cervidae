f6be0c2097a1b158ec522fc1d066a41543c6baa3
Steven Ayers <steveay99@gmail.com>
2017-03-01 21:10:25 +0000

    removing hidden dirs from git

--- puppet/.tmp/librarian/cache/source/puppet/forge/forgeapi_puppetlabs_com/pcfens-filebeat/0.10.4/filebeat/manifests/install/linux.pp
+++
@@ -1,5 +0,0 @@
-class filebeat::install::linux {
-  package {'filebeat':
-    ensure => $filebeat::package_ensure,
-  }
-}
--- puppet/.tmp/librarian/cache/source/puppet/forge/forgeapi_puppetlabs_com/pcfens-filebeat/0.10.4/filebeat/manifests/service.pp
+++
@@ -1,7 +0,0 @@
-class filebeat::service {
-  service { 'filebeat':
-    ensure   => $filebeat::service_ensure,
-    enable   => $filebeat::service_enable,
-    provider => $filebeat::service_provider,
-  }
-}
